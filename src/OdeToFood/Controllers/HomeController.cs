e354a029c6c816ac415e0009771342a3c1ded1b5
SeeSharpCode <seesharpcode@gmail.com>
2017-01-11 14:52:31 -0600

    added restaurant details functionality

--- src/OdeToFood/Controllers/HomeController.cs
+++ src/OdeToFood/Controllers/HomeController.cs
@@ -23,5 +23,17 @@ namespace OdeToFood.Controllers
 
             return View(model);
         }
+
+        public IActionResult Details(int id)
+        {
+            var model = _restaurantData.Get(id);
+
+            if (model == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(model);
+        }
     }
 }
--- src/OdeToFood/Services/RestaurantData.cs
+++ src/OdeToFood/Services/RestaurantData.cs
@@ -1,5 +1,6 @@
 ﻿using OdeToFood.Entities;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace OdeToFood.Services
 {
@@ -7,15 +8,18 @@ namespace OdeToFood.Services
     public interface IRestaurantData
     {
         IEnumerable<Restaurant> GetAll();
+        Restaurant Get(int id);
     }
 
     public class InMemoryRestaurantData : IRestaurantData
     {
+        private List<Restaurant> _restaurants;
+
         public InMemoryRestaurantData()
         {
             _restaurants = new List<Restaurant>
             {
-                new Restaurant { Id = 1, Name="The House of Kobe" },
+                new Restaurant { Id = 1, Name="asdfasdf House of Kobe" },
                 new Restaurant { Id = 2, Name="LJ's and the Kat" },
                 new Restaurant { Id = 3, Name="King's Contrivance" }
             };
@@ -26,6 +30,9 @@ namespace OdeToFood.Services
             return _restaurants;
         }
 
-        List<Restaurant> _restaurants;
+        public Restaurant Get(int id)
+        {
+            return _restaurants.FirstOrDefault(r => r.Id == id);
+        }
     }
 }
