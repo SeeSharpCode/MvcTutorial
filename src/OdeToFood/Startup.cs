f84afca669f2d87286c7d263a2b92323b664cdad
SeeSharpCode <seesharpcode@gmail.com>
2017-01-11 10:00:45 -0600

    added MVC components

--- src/OdeToFood/Startup.cs
+++ src/OdeToFood/Startup.cs
@@ -1,34 +1,35 @@
-﻿using System;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using OdeToFood.Services;
 
 namespace OdeToFood
 {
     public class Startup
     {
-        // public IConfiguration Configuration { get; set; }
+        public IConfiguration Configuration { get; set; }
 
         public Startup(IHostingEnvironment env)
         {
-            //var builder = new ConfigurationBuilder()
-            //    .SetBasePath(env.ContentRootPath)
-            //    .AddJsonFile("appsettings.json");
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json");
 
-            //Configuration = builder.Build();
+            Configuration = builder.Build();
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.AddSingleton(Configuration);
-            //services.AddSingleton<Greeter>();
-
-            services.AddMvcCore();
+            services.AddMvc();
+            services.AddSingleton(Configuration);
+            services.AddSingleton<Greeter>();
+            services.AddScoped<IRestaurantData, InMemoryRestaurantData>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,8 +46,8 @@ namespace OdeToFood
                 app.UseExceptionHandler(new ExceptionHandlerOptions { ExceptionHandler = context => context.Response.WriteAsync("Exception, crap!") });
             }
 
-            // app.UseMvcWithDefaultRoute();
-            app.UseMvc(ConfigureRoutes);
+            app.UseMvcWithDefaultRoute();
+            // app.UseMvc(ConfigureRoutes);
 
             app.Run(ctx => ctx.Response.WriteAsync("Not found"));
 
