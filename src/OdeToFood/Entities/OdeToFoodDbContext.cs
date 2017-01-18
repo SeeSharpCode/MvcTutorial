657c101121e87af156336624b015ab981aab660e
SeeSharpCode <seesharpcode@gmail.com>
2017-01-18 12:26:13 -0600

    Entity Framework and migrations

---
+++ src/OdeToFood/Entities/OdeToFoodDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace OdeToFood.Entities
+{
+    public class OdeToFoodDbContext : DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
+
+        public OdeToFoodDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
